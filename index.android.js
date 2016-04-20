368a4d231633680b4d24528ef7af85c10b29fcdb
Adrian Castro <Adrian Castro>
2016-04-19 22:03:28 -0500

    Primer ejemplo View sencillo

---
+++ index.android.js
@@ -0,0 +1,55 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+class AwesomeProject extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <HelloMessage name='medellinJS' />
+      </View>
+    );
+  }
+}
+
+class HelloMessage extends Component {
+  render() {
+    return (
+      <Text>
+          Hola {this.props.name}!
+      </Text>
+    )
+  }
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  }
+});
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
